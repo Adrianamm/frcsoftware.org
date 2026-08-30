729b1b910b83c0bdcfe640065567ecad3898a598
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2026-08-29 22:52:37 -0500

    Stage1a auto + additonal motors (#194)
    ## Description
    
    Adds stage 1a curriculum for programing a simple auto as well as
    progiraming the IntakeLauncher and Feeder motors. Makes progress on #13
    .
    
    Todo:
    - [x]  Video + text on running the simple auto
    - [x]  Talk about FuelSim
    - [x] Update FuelSim to use `periodic()` over `update()`
    - [x]  Video + text on AScope graph with additonal motors
    - [x]  Video + text on Ascope 3d with additional motors
    
    ## Meta
    
    Merge checklist:
    
    - [x] Pull Request title is [short, imperative
    summary](https://cbea.ms/git-commit/) of proposed changes
    - [x] The description documents the _what_ and _why_, including events
    that led to this PR
    - [x] Pull Request has been [linted and
    formatted](https://frcsoftware.org/contribution/styleguide/#linting-and-formatting)
    and follows the
    [styleguide](https://frcsoftware.org/contribution/styleguide)
    
    ---------
    
    Co-authored-by: Adriana <16786568+Adrianamm@users.noreply.github.com>

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -115,14 +115,14 @@ export const sidebarSections: Record<string, SidebarSection[]> = {
                                     label: 'Drivetrain Simulation',
                                     slug: 'learning-course/stage1/stage1a/drivetrain-sim',
                                 },
-                                // {
-                                //     label: 'TBD',
-                                //     slug: 'stage-1a-commands/the-command-body',
-                                // },
-                                // {
-                                //     label: 'TBD',
-                                //     slug: 'stage-1a-commands/commands-and-mechanisms',
-                                // },
+                                {
+                                    label: 'Simple Auto',
+                                    slug: 'learning-course/stage1/stage1a/simple-auto',
+                                },
+                                {
+                                    label: 'Additional Motors',
+                                    slug: 'learning-course/stage1/stage1a/kitbot-additional-motors',
+                                },
                             ],
                         },
                         {
