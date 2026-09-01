7406f761008561096b9d6188c18dca0c01f8fcd3
Adriana <16786568+Adrianamm@users.noreply.github.com>
2026-09-01 00:26:07 -0400

    rough draft

--- examples/stage0/snippets/src/Debugging.java
+++ examples/stage0/snippets/src/Debugging.java
@@ -14,4 +14,22 @@ void main() {
     int number = 4;
     Systme.out.println(number); // <- System is misspelled!
     // [/syntaxError2]
+
+    //[logicError1]
+    int width = 2;
+    int height = 5;
+    int area = width + height; 
+    System.out.println("Area of the rectangle is " + area);
+    //[/logicError1]
+
+    // [logicError2]
+    double alarmTime = 600; //This is 6:00AM, the time the alarm should go off
+    boolean makeNoise; //If the alarm makes a noise or not
+
+    if (alarmTime == 600) { // If alarm time is equal to 6:00AM
+        makeNoise = false; // the alarm makes no noise 
+    } else { // If the alarm time is anything but 6:00AM
+        makeNoise = false; // the alarm makes no noise
+    }
+    // [/logicError2]
 }
