217a34987a0aee328a56bf16008a31672d89fdc7
Daniel Chen <108989218+Daniel1464@users.noreply.github.com>
2026-09-06 13:57:52 -0400

    Stage1b educators guide (#248)
    ## Meta
    
    Merge checklist:
    
    - [X] Pull Request title is [short, imperative
    summary](https://cbea.ms/git-commit/) of proposed changes
    - [X] The description documents the _what_ and _why_, including events
    that led to this PR
    - [X] Pull Request has been [linted and
    formatted](https://frcsoftware.org/contribution/styleguide/#linting-and-formatting)
    and follows the
    [styleguide](https://frcsoftware.org/contribution/styleguide)
    
    ---------
    
    Co-authored-by: Adriana <16786568+Adrianamm@users.noreply.github.com>

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -264,7 +264,10 @@ export const sidebarSections: Record<string, SidebarSection[]> = {
                 { label: 'Documentation', slug: 'resources/docs' },
                 { label: 'Hardware', slug: 'resources/hardware-intro' },
                 { label: 'Examples', slug: 'resources/examples' },
-                { label: 'Glossary', slug: 'resources/glossary' },
+                {
+                    label: 'Transitioning to Commands V3',
+                    slug: 'resources/transitioning-to-commands-v3',
+                },
             ],
         },
     ],
@@ -356,6 +359,10 @@ export const sidebarSections: Record<string, SidebarSection[]> = {
                 {
                     label: 'Documentation',
                     slug: 'resources/docs',
+                },
+                {
+                    label: 'Transitioning to Commands V3',
+                    slug: 'resources/transitioning-to-commands-v3',
                 },
             ],
         },
